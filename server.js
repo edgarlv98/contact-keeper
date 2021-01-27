29dace84dd60b6744f0cb0b57b667f1074f7e7d3
Edgar Lopez <edgarlv_1998@hotmail.com>
2021-01-27 11:46:11 -0600

    backend setup complete

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the contact keeper API'}));
+connectDB();
+
+app.use(express.json({ extended: false }))
+
+app.get('/',(req, res) => res.json({msg: 'Welcome to the contact keeper API'}));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
